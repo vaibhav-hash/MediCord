6311d41c71c76ed35fc62d0ca5bd293bf1458e7f
vaibhav-hash <techfashion0101@gmail.com>
2021-04-12 21:22:18 +0530

    final

---
+++ server/routes/doctorAuth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const requireDoctorLogin = require("../middleware/requireDoctorLogin");
+const {
+    signup,
+    signin,
+    dashboard,
+    getAllDoctors
+} = require("../controllers/doctorAuth");
+const { body } = require("express-validator");
+
+router.post("/signup", [
+    body("name", "Please provide a valid name").not().isEmpty(),
+    body("email", "Please provide a valid email address").isEmail(),
+    body("department", "Please provide a valid department").not().isEmpty(),
+    body(
+      "password",
+      "Please provide a password altleast 6 characters long"
+    ).isLength({ min: 6 }),
+  ],signup);
+
+
+router.post(
+    "/signin",
+    [
+      body("email", "Please provide a valid email address").isEmail(),
+      body("password", "Please provide a password").not().isEmpty(),
+    ],
+    signin
+  );
+
+router.get("/dashboard",requireDoctorLogin,dashboard);
+
+// routes.get("/",getAllDoctors);
+router.get("/",getAllDoctors);
+module.exports = router;
